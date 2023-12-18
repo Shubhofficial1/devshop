0e989dc7ce30224fa003e480df87938d201e9ec1
shubham kumar <shubhofficial1@gmail.com>
2023-12-18 21:56:19 +0530

    Fetch Products using backend api's

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.get("/api/products/:id", (req, res) => {
   res.send(product);
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);

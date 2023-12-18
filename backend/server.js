34caeefc092a0611ea2173817ad52cadd47a8ccf
shubham kumar <shubhofficial1@gmail.com>
2023-12-19 01:20:22 +0530

    Connecting with mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import products from "./data/products.js";
 import dotenv from "dotenv";
+import connectDb from "./config/db.js";
 dotenv.config();
+connectDb();
 
 const app = express();
 app.use(express.json());

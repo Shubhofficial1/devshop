84f44824fc18dcfafc6b0e0fcf84f4d0c6242edf
shubham kumar <shubhofficial1@gmail.com>
2023-12-18 12:57:15 +0530

    Added Custom Bootstrap Theme

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "./assets/styles/index.css";
+import "./assets/styles/bootstrap.custom.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(

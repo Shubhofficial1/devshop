d6e54418501bdcafaa05b2f93f0b84026c1a6baa
shubham kumar <shubhofficial1@gmail.com>
2023-12-20 01:54:16 +0530

    Fetch Products & Product Details Using Redux

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,5 @@
-export const BASE_URL =
-  process.env.NODE_ENV === "production" ? "http://localhost:5000" : "";
-export const PRODUCT_URL = "api/v1/products";
-export const USERS_URL = "api/v1/users";
-export const ORDERS_URL = "api/v1/orders";
-export const PAYPAL_CONFIG = "api/v1/config/paypal";
+export const BASE_URL = "";
+export const PRODUCTS_URL = "/api/v1/products";
+export const USERS_URL = "/api/v1/users";
+export const ORDERS_URL = "/api/v1/orders";
+export const PAYPAL_CONFIG = "/api/v1/config/paypal";
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,32 +1,29 @@
-import React, { useState, useEffect } from "react";
 import { Row, Col, Container } from "react-bootstrap";
 import Product from "../components/Product";
-import axios from "axios";
+import { useGetProductsQuery } from "../slices/productsApiSlice";
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get("/api/v1/products");
-      setProducts(data);
-    };
-
-    fetchProducts();
-  }, []);
-
+  const { data: products, isLoading, error } = useGetProductsQuery();
   return (
     <>
-      <h1>Latest Products</h1>
-      <Container>
-        <Row>
-          {products?.map((product) => (
-            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-              <Product product={product} />
-            </Col>
-          ))}
-        </Row>
-      </Container>
+      {isLoading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <div>{error?.data?.message || error?.error}</div>
+      ) : (
+        <>
+          <h1>Latest Products</h1>
+          <Container>
+            <Row>
+              {products?.map((product) => (
+                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                  <Product product={product} />
+                </Col>
+              ))}
+            </Row>
+          </Container>
+        </>
+      )}
     </>
   );
 };

3229eb8bd0b6895077e3402198bdd09a50be6e16
shubham kumar <shubhofficial1@gmail.com>
2023-12-19 14:31:59 +0530

    Getting Products from Database

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,28 @@
+import express from "express";
+import asyncHandler from "../middlewares/asyncHandler.js";
+import Product from "../model/productModel.js";
+
+const router = express.Router();
+
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const product = await Product.find({});
+    res.json(product);
+  })
+);
+
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+      return res.status(200).json(product);
+    }
+
+    res.status(404).status({ message: "Product not found" });
+  })
+);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import products from "./data/products.js";
+import productRoutes from "./routes/productRoutes.js";
 import dotenv from "dotenv";
 import connectDb from "./config/db.js";
 dotenv.config();
@@ -12,14 +12,7 @@ app.get("/", (req, res) => {
   res.send("Api is running...");
 });
 
-app.get("/api/products", (req, res) => {
-  res.send(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.send(product);
-});
+app.use("/api/v1/products", productRoutes);
 
 const PORT = process.env.PORT || 5000;
 

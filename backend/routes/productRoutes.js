8c3b0b953344c51f893357b42e9776a9f0923956
shubham kumar <shubhofficial1@gmail.com>
2024-01-07 22:31:27 +0530

    Custom Errors & checking for valid objectId

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -10,14 +10,15 @@ import {
 } from "../controllers/productControllers.js";
 const router = express.Router();
 import { protect, admin } from "../middlewares/authMiddleware.js";
+import checkObjectId from "../middleware/checkObjectId.js";
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
 router.get("/top", getTopProducts);
 router
   .route("/:id")
-  .get(getProductById)
-  .put(protect, admin, updateProduct)
-  .delete(protect, admin, deleteProduct);
-router.route("/:id/reviews").post(protect, createProductReview);
+  .get(checkObjectId, getProductById)
+  .put(protect, admin, checkObjectId, updateProduct)
+  .delete(protect, admin, checkObjectId, deleteProduct);
+router.route("/:id/reviews").post(protect, checkObjectId, createProductReview);
 
 export default router;

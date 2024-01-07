ddcd624def80e98d8aa444ce1474669d16ec47ed
shubham kumar <shubhofficial1@gmail.com>
2024-01-07 22:44:48 +0530

    Reset Cart on logout

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -6,6 +6,7 @@ import { useNavigate } from "react-router-dom";
 import { useLogoutMutation } from "../slices/usersApiSlice";
 import { logout } from "../slices/authSlice";
 import SearchBox from "./SearchBox";
+import { resetCart } from "../slices/cartSlice";
 
 const Header = () => {
   const { cartItems } = useSelector((state) => state.cart);
@@ -21,6 +22,8 @@ const Header = () => {
     try {
       await logoutApiCall().unwrap();
       dispatch(logout());
+      dispatch(resetCart());
+
       navigate("/login");
     } catch (err) {
       console.error(err);
